1ac8a6433626a7f9eb2b3ca37ff0bd81d1c6317d
Benknightdark <moon791017@gmail.com>
2017-10-08 17:15:03 +0800

    add three ng on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressComponent } from './progress/progress.component';
 import { CountdownComponent } from './countdown/countdown.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,8 @@ import { CountdownComponent } from './countdown/countdown.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy, OnChanges } from '@angular/core';
 
 
 @Component({
@@ -6,30 +6,47 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent implements OnInit {
+export class CountdownComponent implements OnInit,OnDestroy, OnChanges {
   @Input() Init: number = null;
   @Output() onDecrease = new EventEmitter<number>();
   @Output() onComplete = new EventEmitter<void>();
   Counter: number = 0;
+  CountDownTimeRef:any=null;
   constructor() { }
 
   ngOnInit() {
-    this.startCountdown()
+    //this.startCountdown()
+  }
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.ClearTimeout();
+  }
+  ngOnChanges(changes) {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log(changes)
+   // this.doCountdown()
   }
   startCountdown() {
-    console.log(this.Init)
-    console.log(this.Init)
 
     if (this.Init<100 ) {
+      this.ClearTimeout();
       this.Counter = this.Init;
       this.doCountdown();
     }
   }
   doCountdown() {
-    setTimeout(() => {
+   this.CountDownTimeRef= setTimeout(() => {
       this.Counter = this.Counter + 1;
       this.processCountdown();
     }, 1000);
+  }
+  ClearTimeout(){
+    if(this.CountDownTimeRef){
+      clearTimeout(this.CountDownTimeRef);
+      this.CountDownTimeRef=null;
+    }
   }
   processCountdown() {
     this.onDecrease.emit(this.Counter);
